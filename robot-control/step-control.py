42d26764d31b6111a56263e2d9a45c6a257a63b5
TyCary <tywincary@gmail.com>
2023-01-31 23:38:49 -0800

    fixed/tested print file

--- robot-control/step-control.py
+++ robot-control/step-control.py
@@ -17,9 +17,10 @@ ser = serial.Serial(comChannel, Baudrate, timeout=to)
 tm.sleep(0.5)  # Wait for com
 ser.reset_input_buffer()
 ser.reset_output_buffer()
-tm.sleep(2)
+tm.sleep(0.5)
+print("Communication Ready")
 ser.flush()
-tm.sleep(3)
+tm.sleep(2)
 
 # Main
 infile = open('stringart/output/NailOutput.txt', 'r')
@@ -29,16 +30,17 @@ nail = commands.split(',')
 ser.write((str(gearRatio)+"\n").encode('utf-8'))
 ser.reset_output_buffer()
 print("Gear Ratio Set")
-tm.sleep(2)
+tm.sleep(1)
 
 ser.write(("402\n").encode('utf-8'))
 print("Homing")
-tm.sleep(5)
+tm.sleep(5)  # Wait time for Homing
 ser.reset_output_buffer()
 
 prevNail = 0
 # Send nails to Arduino
-for i in range(10):
+print("Printing\n")
+for i in range(30):
     print("Iteration: " + str(i) + " | current nail: " + nail[i])
     ser.write((nail[i]+"\n").encode('utf-8'))
     # tm.sleep(1.2*gearRatio)
@@ -47,8 +49,12 @@ for i in range(10):
         msg = ser.read().decode('utf-8')
     ser.reset_input_buffer()
     ser.reset_output_buffer()
+    tm.sleep(0.1)
     prevNail = int(nail[i])
 
-
+print("\nPrint Complete!\n")
+tm.sleep(1)
+ser.write(("402\n").encode('utf-8'))  # Home Steppers
+tm.sleep(2)
 ser.write(("401\n").encode('utf-8'))  # Turn off Steppers
 infile.close()
