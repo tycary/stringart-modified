50826f08c2d96d311ea9b6b7085b6001274a5d73
Tywin Cary <tywincary@gmail.com>
2023-10-03 23:32:44 -0700

    test1

--- robot-control/arduinoProgram/arduinoProgram.ino
+++ robot-control/arduinoProgram/arduinoProgram.ino
@@ -1,17 +1,21 @@
-#include <Adafruit_MotorShield.h>
+#include <AccelStepper.h>
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Creating motorshield object
+#define motor1dirPin 2
+#define motor1stepPin 3
+#define motor2dirPin 4
+#define motor2stepPin 5
+#define motorInterfaceType 1
 
-// Stepper Motor Object
-Adafruit_StepperMotor *motor1 = AFMS.getStepper(200, 2);
-Adafruit_StepperMotor *motor2 = AFMS.getStepper(200, 1);
+// Stepper Motor Objects
+AccelStepper motor1 = AccelStepper(motorInterfaceType, motor1stepPin, motor1dirPin);
+AccelStepper motor2 = AccelStepper(motorInterfaceType, motor2stepPin, motor2dirPin);
 
 // General Variables
 String instructions;
 int instr, curPos, baudrate, initial, trueinst, nailos; // NailOffset == boolean is-Odd
 float gearRatio;
 
-#define MAXNAIL 399 //Change for num of nodes * 2 - 1
+#define MAXNAIL 399 // Change for num of nodes * 2 - 1
 #define CW BACKWARD
 #define CCW FORWARD
 
@@ -19,9 +23,11 @@ void setup()
 {
   Serial.begin(115200); // Common rates: 9600, 19200, 38400, *115200*, 230400
   AFMS.begin();
-  // steps per second; Max is 200sps (1 rps)
-  motor1->setSpeed(200);
-  motor2->setSpeed(6);
+  // steps per second; Max is 500sps (2.5 rps)
+  motor1.setSpeed(500);
+  motor2.setSpeed(6);
+  motor1.setAcceleration(250);
+  motor2.setAcceleration(100);
   instr = 0;
   gearRatio = 1;
   initial = 0;
@@ -57,35 +63,22 @@ void loop()
       }
       else if (instr == 401)
       { // Pause
-        motor1->release();
-        motor2->release();
       }
       else if (instr == 402)
       { // Home
-        curPos = moveMotor(0, curPos, gearRatio);
+        curPos = 0;
+        motor1.moveTo(0);
       }
       else if (instr == 403)
-      { //Backward 360
-        motor1->step(200 * gearRatio, CCW, DOUBLE);
+      { // Backward 360
+        motor1.moveTo(0);
       }
       else if (instr == 405)
-      { //Forward 360
-        motor1->step(200 * gearRatio, CW, DOUBLE);
+      { // Forward 360
+        motor1.moveTo(200 * gearRatio);
       }
-      else if(instr == 406) 
+      else if (instr == 406)
       { // Test Pattern
-        for (int i = 0; i<100; ++i)
-        {
-          if (i%2 == 0)
-          {
-            motor2->step(10,BACKWARD, DOUBLE);
-            motor1->step(gearRatio, CW, DOUBLE);
-            motor2->step(10, FORWARD, DOUBLE);
-          }
-          //delay(50);
-          motor1->step(35*gearRatio, CW, DOUBLE);
-          delay(50);
-        }
       }
       else if (instr <= MAXNAIL && instr >= 0)
       {
@@ -99,19 +92,22 @@ void loop()
           trueinst = (instr + 1) / 2;
           nailos = 1;
         }
-        curPos = moveMotor(trueinst, curPos, gearRatio); // Move wheel to position
-        motor2->step(11, BACKWARD, DOUBLE);         // Move threader down
-        delay(100); //testing delays
+        curPos = calcTarget(trueinst, curPos, gearRatio); // Move wheel to position
+        motor1.moveTo(curPos * gearRatio);
+        motor2.moveTo(11); // Move threader down
+        delay(100);        // testing delays
         if (nailos == 0)
         {
-          curPos = moveMotor(trueinst + 1, curPos, gearRatio); // Move wheel to next nail
+          curPos = calcTarget(trueinst + 1, curPos, gearRatio); // Move wheel to next nail
+          motor1.moveTo(curPos * gearRatio);
         }
         else
         {
-          curPos = moveMotor(trueinst - 1, curPos, gearRatio); // Move wheel to next nail
+          curPos = calcTarget(trueinst - 1, curPos, gearRatio); // Move wheel to next nail
+          motor1.moveTo(curPos * gearRatio);
         }
-        delay(100); //testing delays
-        motor2->step(11, FORWARD, DOUBLE); // Move threader up
+        delay(100);       // testing delays
+        motor2.moveTo(0); // Move threader up
         Serial.println("x");
       }
     }
@@ -124,7 +120,7 @@ void loop()
 }
 
 // Moves stepper to target position and returns target
-int moveMotor(int target, int cur, float ratio)
+int calcTarget(int target, int cur, float ratio)
 {
   if (target == 199 + 1)
   {
@@ -134,23 +130,5 @@ int moveMotor(int target, int cur, float ratio)
   {
     target = 199;
   }
-  int difference = target - cur;
-  if (difference > 100)
-  {
-    motor1->step(int(abs(difference - 200) * ratio), FORWARD, DOUBLE);
-  }
-  else if (difference < -100)
-  {
-    motor1->step(int((difference + 200) * ratio), BACKWARD, DOUBLE);
-  }
-  else if (difference > 0)
-  {
-    motor1->step(int(difference * ratio), BACKWARD, DOUBLE);
-  }
-  else if (difference < 0)
-  {
-    motor1->step(int(abs(difference) * ratio), FORWARD, DOUBLE);
-  }
-  delay(5);
   return target;
 }
